00c259980c5ee25efa79d3740d88211092257810
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2022-11-23 17:40:18 -0500

    WIP google map with search

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import GoogleMap from "./GoogleMap"
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/map">
+          <GoogleMap />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
